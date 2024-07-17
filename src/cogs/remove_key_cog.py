67f68a507f56b2181cef97029ce4c231ec3a2c9c
darkharasho <project96+github@gmail.com>
2024-07-17 10:37:12 -0700

    missing bracket

--- src/cogs/remove_key_cog.py
+++ src/cogs/remove_key_cog.py
@@ -61,7 +61,7 @@ class RemoveKeyCog(commands.Cog):
         if type(event) == discord.Interaction:
             await event.response.defer()
             embed = discord.Embed(title="Remove API Key", description="")
-            member = Member.select().where(Member.discord_id == interaction.user.id) & (Member.guild_id == interaction.guild.id)).first()
+            member = Member.select().where((Member.discord_id == interaction.user.id) & (Member.guild_id == interaction.guild.id)).first()
             api_key = ApiKey.select().where((ApiKey.id == event.data['values'][0]) & (ApiKey.guild_id == interaction.guild.id)).first()
             was_primary = api_key.primary
             if api_key.member == member:
