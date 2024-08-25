2ea067109cde7eaf4d2af605ffa2072dcbc5d22a
darkharasho <project96+github@gmail.com>
2024-08-25 15:47:26 -0700

    sync commands every hour

---
+++ src/tasks/reload_cogs_task.py
@@ -0,0 +1,34 @@
+import pdb
+
+import discord
+import requests
+from datetime import datetime, timedelta, timezone
+from discord.ext import commands, tasks
+from config import settings
+from src.lib.logger import logger
+from src.models.config import Config
+from src import helpers
+
+
+class ReloadCogsTask(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.reload_cogs.start()
+
+    def cog_unload(self):
+        self.reload_cogs.cancel()
+
+    @tasks.loop(hours=1)
+    async def reload_cogs(self):
+        print("[COG SYNC]".ljust(20) + f"🟢 STARTED")
+        try:
+            await self.bot.tree.sync()
+            print("[COG SYNC]".ljust(20) + f"🟢 COMPLETE")
+        except Exception as e:
+            print("[COG SYNC]".ljust(20) + f"🔴 FAILED")
+            print(e)
+
+
+async def setup(bot):
+    for guild in bot.guilds:
+        await bot.add_cog(ReloadCogsTask(bot), guild=guild, override=True)
